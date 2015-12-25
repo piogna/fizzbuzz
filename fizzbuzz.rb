0b6b38129b061a0838ba2566e375651a038c6b17
Paul Iogna <paul.iogna@gmail.com>
2015-12-25 01:51:09 -0500

    fizzbuzz complete

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+n = ARGV[0].to_i
+count =
+for i in 1..n
+  if i % 3 == 0 && i % 5 == 0
+    puts "fizzbuzz"
+  elsif i % 3 == 0
+    puts "fizz"
+  elsif i % 5 == 0
+    puts "buzz"
+  else
+    puts "#{i}"
+  end
+end
